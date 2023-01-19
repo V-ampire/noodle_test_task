550bddf8e5af1aa27baf33af9c4032900c569288
v-ampire <webjob010@gmail.com>
2023-01-19 20:44:09 +0600

    fix model id field

--- vk_integration/models.py
+++ vk_integration/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class VkGroup(models.Model):
     """Database model for vk group."""
 
-    group_id = models.BigIntegerField(unique=True)
+    id = models.BigIntegerField(primary_key=True)
     name = models.CharField(max_length=264)
     users_count = models.BigIntegerField()
 
     def __str__(self):
-        return f"{self.name} ({self.group_id})"
+        return f"{self.name} ({self.id})"
