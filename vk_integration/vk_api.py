8723ffdfa7d5529ec2a46f3d28ee6a3d0e0a8601
v-ampire <webjob010@gmail.com>
2023-01-20 23:56:21 +0600

    add api method

--- vk_integration/vk_api.py
+++ vk_integration/vk_api.py
@@ -1,4 +1,5 @@
 import aiohttp
+from pydantic import ValidationError
 
 from vk_integration.shemas import VkGroupSchema
 
@@ -9,9 +10,7 @@ class SingletonAPI(type):
 
     def __call__(cls, *args, **kwargs):
         if cls not in cls._api_instances:
-            print(f'New API {cls}')
             cls._api_instances[cls] = super(SingletonAPI, cls).__call__(*args, **kwargs)
-        print(f'API {cls} exists')
         return cls._api_instances[cls]
 
 
@@ -36,6 +35,19 @@ class VkAPI(metaclass=SingletonAPI):
             Authorization=f"Bearer {self.access_token}"
         )
 
+    async def _make_request(self, url, params=None, headers=None):
+        if not headers:
+            headers = {}
+        headers.update(self._get_auth_headers())
+        async with self._get_session() as session:
+            async with session.get(url, params=params, headers=headers) as resp:
+                response_data = await resp.json()
+
+        if not response_data.get('response') or response_data.get('error'):
+            raise ValidationError(f"Error in response from VK API {response_data}")
+
+        return response_data.get('response')
+
     async def get_group_info(self, group_id: int, fields: str = 'id,members_count,name', **opts) -> VkGroupSchema:
         """
         Get VK group info.
@@ -51,9 +63,28 @@ class VkAPI(metaclass=SingletonAPI):
         )
         params.update(opts)
 
-        async with self._get_session() as session:
-            async with session.get(url, params=params, headers=self._get_auth_headers()) as resp:
-                resp_data =  await resp.json()
+        resp_data = await self._make_request(url, params)
 
-        return VkGroupSchema.from_response(resp_data)
+        return VkGroupSchema.from_response(resp_data[0])
+
+    async def get_group_batch_info(self, group_ids: list[int], fields: str = 'id,members_count,name', **opts
+                                   ) -> list[VkGroupSchema]:
+        """
+        Get VK group info for list of groups.
+
+        :param group_ids: List of VK group ID.
+        :param fields: Comma separated fields as string. Default: id,members_count,name.
+        """
+        url = f"{self.api_base_url}/groups.getById"
+        params = dict(
+            group_ids=','.join([str(g_id) for g_id in group_ids]),
+            fields=fields,
+            v=self.api_version,
+        )
+        params.update(opts)
+
+        groups_data = await self._make_request(url, params)
+
+        return [VkGroupSchema.from_response(group_data) for group_data in groups_data]
+
 
