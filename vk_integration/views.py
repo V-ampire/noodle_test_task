f98654ba06ee62fc924d936daf8c28ac72011d62
v-ampire <webjob010@gmail.com>
2023-01-21 00:01:13 +0600

    exc in view

--- vk_integration/views.py
+++ vk_integration/views.py
@@ -1,15 +1,25 @@
+from logging import getLogger
+
 from django.http import JsonResponse
 from django.views import View
 
 from vk_integration.services import VkGroupCompositeProvider
 
 
+logger = getLogger(__name__)
+
+
 class GroupView(View):
 
     async def get(self, request, group_id, **kwargs):
         """Get vk group info."""
-        provider = VkGroupCompositeProvider()
-        group = await provider.get_by_id(group_id)
-        if group:
-            return JsonResponse(dict(group))
-        return JsonResponse({'error': 'Not Found'}, status=404)
+        try:
+            provider = VkGroupCompositeProvider()
+            group = await provider.get_by_id(group_id)
+            if group:
+                return JsonResponse(dict(group))
+            return JsonResponse({'error': 'Not Found'}, status=404)
+        except Exception as exc:
+            logger.error(f'Error in GroupView, {exc}')
+            return JsonResponse({'error': 'Server Error'}, status=500)
+
